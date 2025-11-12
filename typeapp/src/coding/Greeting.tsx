b56180ef3bbc7e0c4a0bbd1f5cb84f1e6e3c29ba
akshay <ametange9146@gmail.com>
2025-11-12 20:44:25 +0530

    object prop type

--- typeapp/src/coding/Greeting.tsx
+++ typeapp/src/coding/Greeting.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react"
+
 type nameProp={
     name:string,
     children:React.ReactNode;
 }
-
+type objProps={
+    name:string,
+    age:number
+}
 export default function Greeting({name,children}:nameProp){
+    const [count,setCount]=useState<objProps>({name:'akshay',age:21});
+    const handleChange=()=>{
+        setCount((prev)=>{return{
+            ...prev,
+            name:'abhi'
+
+        }})
+    }
     return(
         <>
-        hello age and my name is - {name} {children}
+        hello my name is - {count.name} {children}
+        <button onClick={handleChange}>change</button>
         </>
     )
 }
